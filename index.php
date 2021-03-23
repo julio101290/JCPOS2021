bfb298299ff99b8325d8debdd6c74cace19fc88d
juliocesar <juliocesar@juliocesar-Satellite-L50-A>
2021-03-22 23:20:29 -0700

    Cambio para mostrar errores 2 ini_set('display_errors', 1);

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 //error_reporting(E_ERROR | E_PARSE);
+
+ini_set('display_errors', 1);
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/categorias.controlador.php";
