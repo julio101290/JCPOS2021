4c85895a02c2cf8cd7e012544908e274f7db5e29
juliocesar <juliocesar@juliocesar-Satellite-L50-A>
2021-04-04 23:50:50 -0600

    CAMBIO TCPDF

--- extensiones/tcpdf/include/tcpdf_fonts.php
+++ extensiones/tcpdf/include/tcpdf_fonts.php
@@ -44,8 +44,37 @@
  * @package com.tecnick.tcpdf
  * @version 1.1.0
  * @author Nicola Asuni - info@tecnick.com
+ * 
  */
+
+
+
+
 class TCPDF_FONTS {
+    
+    
+    public static function unichr($c, $unicode=true) {
+        if(is_numeric($c))
+        {
+            if (!$unicode) {
+                return chr($c);
+            } elseif ($c <= 0x7F) {
+                // one byte
+                return chr($c);
+            } elseif ($c <= 0x7FF) {
+                // two bytes
+                return chr(0xC0 | $c >> 6).chr(0x80 | $c & 0x3F);
+            } elseif ($c <= 0xFFFF) {
+                // three bytes
+                return chr(0xE0 | $c >> 12).chr(0x80 | $c >> 6 & 0x3F).chr(0x80 | $c & 0x3F);
+            } elseif ($c <= 0x10FFFF) {
+                // four bytes
+                return chr(0xF0 | $c >> 18).chr(0x80 | $c >> 12 & 0x3F).chr(0x80 | $c >> 6 & 0x3F).chr(0x80 | $c & 0x3F);
+            } else {
+                return '';
+            }
+        }
+    }
 
 	/**
 	 * Static cache used for speed up uniord performances
