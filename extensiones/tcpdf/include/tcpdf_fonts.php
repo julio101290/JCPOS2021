545d58bb1d33aecf278c4fffabb644634718e65a
juliocesar <juliocesar@juliocesar-Satellite-L50-A>
2021-04-04 23:54:17 -0600

    CAMBIO TCPDF

--- extensiones/tcpdf/include/tcpdf_fonts.php
+++ extensiones/tcpdf/include/tcpdf_fonts.php
@@ -1505,6 +1505,7 @@ class TCPDF_FONTS {
 	 * @public static
 	 */
 	public static function unichr($c, $unicode=true) {
+            $c = intval($c);
 		if (!$unicode) {
 			return chr($c);
 		} elseif ($c <= 0x7F) {
