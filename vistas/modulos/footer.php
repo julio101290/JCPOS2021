d25b38e2d2043c1a8676c0c061d4d88a4c7764d0
juliocesar <juliocesar@juliocesar-Satellite-L50-A>
2021-07-25 10:43:44 -0600

    CAMBIO COMPLETO PARA ACTUALIZAR DESDE EL GITHUB MAS INFORMACION AQUI https://www.youtube.com/watch?v=h-g1H-nnLlQ

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2020 <a href="https://cesarsystems.com.mx/" target="_blank">JCLEYVA SOFTWARE VERSION 20210509</a>.</strong>
+	<strong>Copyright &copy; 2020 <a href="https://cesarsystems.com.mx/" target="_blank">JCLEYVA SOFTWARE VERSION 20210725</a>.</strong>
 
 	Todos los derechos reservados.
 
