90f073c9cb793eb5b8f8b03b8ed90d74c2a4482c
juliocesar <juliocesar@juliocesar-Satellite-L50-A>
2021-07-25 22:20:50 -0600

    CORRECCION AL GUARDAR LA VENTA CORRER ESTOS COMANDOS EN MYSQL
    ALTER TABLE `ventas` ADD `UUID` VARCHAR(36) NULL AFTER `Observaciones`; 
    update ventas set uuid=uuid();
    ALTER TABLE `ventas` ADD UNIQUE( `UUID`); 
    ALTER TABLE `ventas` CHANGE `UUID` `UUID` VARCHAR(36) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL;

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2020 <a href="https://cesarsystems.com.mx/" target="_blank">JCLEYVA SOFTWARE VERSION 20210725A</a>.</strong>
+	<strong>Copyright &copy; 2020 <a href="https://cesarsystems.com.mx/" target="_blank">JCLEYVA SOFTWARE VERSION 20210725B</a>.</strong>
 
 	Todos los derechos reservados.
 
