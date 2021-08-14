d49a8b508dd3576331f1bc2eadfab7647bfed463
juliocesar <juliocesar@juliocesar-Satellite-L50-A>
2021-08-13 23:17:39 -0600

    Cambios Vision Mision

--- vistas/modulos/acercaDe.php
+++ vistas/modulos/acercaDe.php
@@ -56,13 +56,8 @@
                                     </div>
                                     <div id="collapseOne" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
                                         <div class="box-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                                            wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                                            eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                            assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                            nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                            labore sustainable VHS.
+                                            JCPOS es un software que tiene como misión ofrecer una alternativa libre a las pequeñas empresas y negocios que apenas 
+                                            van empezando incorporarse en el mercado local a tener control automatico básico de las ventas e inventarios.
                                         </div>
                                     </div>
                                 </div>
@@ -76,13 +71,8 @@
                                     </div>
                                     <div id="collapseTwo" class="panel-collapse collapse" aria-expanded="false">
                                         <div class="box-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                                            wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                                            eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                            assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                            nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                            labore sustainable VHS.
+                                            Tenemos la visión de que el software sea libre para que los estudiantes en informatica lo estudien, modifiquen y den soporte y que ellos mismo
+                                            se beneficien economicamente del soporte y desarrollo extra que otorgen, por eso el codigo es de libre distribución.
                                         </div>
                                     </div>
                                 </div>
@@ -96,13 +86,25 @@
                                     </div>
                                     <div id="collapseThree" class="panel-collapse collapse" aria-expanded="false">
                                         <div class="box-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                                            wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                                            eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                            assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                            nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                            labore sustainable VHS.
+                                            El software libre que ha sido liderado por Richard Stallman tiene una intención ética y moral, y se basa en las siguientes cuatros libertades que son la declaración de principios de este movimiento:
+
+                                            1. La libertad de usar el programa con cualquier propósito.
+                                            2. La libertad de estudiar cómo funciona el programa y modificarlo, adaptándolo a tus necesidades.
+                                            3. La libertad de distribuir copias del programa, con lo cual puedes ayudar a tu prójimo.
+                                            4. La libertad de mejorar el programa y hacer públicas esas mejoras a los demás, de modo que toda la comunidad se beneficie.
+
+                                            En un mundo tan competitivo y lucrativo como el que vivimos, el software libre aporta una serie de valores de los cuales destacamos:
+
+                                            - fomenta la colaboración
+                                            - incita a trabajar en comunidades participativas
+                                            - motiva la creación del conocimiento compartido
+                                            - promueve la filosofía del trabajo en equipo
+                                            - estimula la transparencia al hacer el código fuente libremente accesible
+                                            - anima la libre adaptación de las aplicaciones a las necesidades del usuario.
+
+                                            El software libre no tiene porqué ser necesariamente gratuito. Y que algo sea de acceso público, no quiere decir que no se remunere el trabajo realizado, aunque en general la distribución del mismo es gratuita.
+
+                                            Finalmente, destacamos la activa participación de las comunidades de usuarios y usuarias que sirven para evaluar, mejorar, actualizar y desarrollar las aplicaciones creadas.
                                         </div>
                                     </div>
                                 </div>
